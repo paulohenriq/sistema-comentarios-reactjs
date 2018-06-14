5764e7b36eb602e9d33fbe758465b4dac80c2433
Paulo Henrique <paulohenriq.dev@gmail.com>
2018-06-14 01:27:22 -0300

    Ajustes nos arquivos iniciais do projeto

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap-css-only';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-header">          
+          <h1 className="App-title">Sistema de Comentários</h1>
+          <p><button className="btn btn-info">Teste</button></p>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
